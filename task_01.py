9aa21b43d0897ff0310b801628f0ea4994c5787b
vladdy-b <vladyslav.boikov12@gmail.com>
2025-11-08 01:38:09 +0200

    add first task

---
+++ task_01.py
@@ -0,0 +1,32 @@
+def normalize_salary(value: float):
+    return int(value) if value.is_integer() else value
+
+
+def total_salary(path):
+    total = 0
+    workers = 0
+    try:
+        with open(path, encoding='utf-8') as file:
+            for line_number, line in enumerate(file):
+                try:
+                    _, salary = line.strip().split(',')
+                    total += float(salary)
+                    workers += 1
+                except ValueError:
+                    print(f'Invalid salary "{salary}" on line {line_number}, skipping this line')
+                    continue
+
+            average = total / workers if workers > 0 else 0
+
+            return normalize_salary(total), normalize_salary(average)
+
+    except FileNotFoundError:
+        print(f'File not found: {path}')
+        return 0,0 
+
+    except Exception as e:
+        print(f'Error: {e} in {path} file')
+        return 0, 0
+
+total, average = total_salary("./salaries.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
